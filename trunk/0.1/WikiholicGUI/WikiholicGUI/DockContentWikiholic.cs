679ed5821181da02e75ea54ad19d0a32ca66cd24
yeoupooh <yeoupooh@8cb1f029-0a3d-0410-a2a4-d11a6d6508d2>
2007-10-22 18:23:12 +0000

    - added buttons.

--- trunk/0.1/WikiholicGUI/WikiholicGUI/DockContentWikiholic.cs
+++ trunk/0.1/WikiholicGUI/WikiholicGUI/DockContentWikiholic.cs
@@ -11,6 +11,18 @@ namespace WikiholicGUI
 {
     public partial class DockContentWikiholic : DockContent
     {
+        public ToolStrip ts { get { return toolStrip1; } }
+
+        public ToolStripButton btAddServer { get { return toolStripButton1; } }
+        public ToolStripButton btEditServer { get { return toolStripButton2; } }
+        public ToolStripButton btRemoveServer { get { return toolStripButton3; } }
+
+        public ToolStripButton btAddWiki { get { return toolStripButton4; } }
+        public ToolStripButton btEditWiki { get { return toolStripButton5; } }
+        public ToolStripButton btRemoveWiki { get { return toolStripButton6; } }
+
+        public TreeView tv { get { return treeViewMain; } }
+
         public DockContentWikiholic()
         {
             InitializeComponent();
