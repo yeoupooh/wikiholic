b432924e41ec1b4dd7e408bde1d414663110f22d
yeoupooh <yeoupooh@8cb1f029-0a3d-0410-a2a4-d11a6d6508d2>
2008-04-07 02:29:18 +0000

    - added initial TracPlugin.   - required test server (TOW from http://sourceforge.net/projects/traconwindows)   - can get all pages.   - can get select page content.

---
+++ Test/TestWikiholicUsingRBPV2/Wikiholic.Trac/TracPlugin.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenCS.Common.Plugin;
+using OpenCS.RBP;
+using OpenCS.Common.Action;
+using System.Diagnostics;
+using WeifenLuo.WinFormsUI.Docking;
+
+namespace Wikiholic.Trac
+{
+    public class TracPlugin : BaseRbpPlugin
+    {
+        public override void Init()
+        {
+            DCTrac dc = new DCTrac();
+            dc.Show(m_rbc.DockPanel, DockState.DockLeft);
+        }
+
+        public override void Deinit()
+        {
+        }
+
+        public override ActionResult HandleAction(IAction action)
+        {
+            return ActionResult.NotHandled;
+        }
+
+        public override string Title
+        {
+            get { return "TracPlugin"; }
+        }
+
+        public override Version Version
+        {
+            get { return new Version("0.1.1.1"); }
+        }
+    }
+}
