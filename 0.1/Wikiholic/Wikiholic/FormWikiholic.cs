7d45ad7d4f5be9916d9d8beec83fcfe823e62ee2
yeoupooh <yeoupooh@8cb1f029-0a3d-0410-a2a4-d11a6d6508d2>
2008-10-17 04:16:21 +0000

    * initial commit of Wikiholic 0.1.

---
+++ 0.1/Wikiholic/Wikiholic/FormWikiholic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Wikiholic
+{
+    public partial class FormWikiholic : Form
+    {
+        public FormWikiholic()
+        {
+            InitializeComponent();
+        }
+    }
+}
