ab041dcd148cc2eb2a1f23129dcbd41d7099d3a6
yeoupooh <yeoupooh@8cb1f029-0a3d-0410-a2a4-d11a6d6508d2>
2009-08-17 05:55:35 +0000

    - initial commit

---
+++ trunk/Test/FreeMind2Wiki/Google.mm
@@ -0,0 +1,10 @@
+<map version="0.8.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1156999061406" ID="Freemind_Link_1379007866" MODIFIED="1156999064875" TEXT="Google">
+<node CREATED="1156999066906" ID="Freemind_Link_685206366" MODIFIED="1156999070859" POSITION="right" TEXT="Video">
+<node CREATED="1156999072468" ID="Freemind_Link_1305806434" MODIFIED="1156999078359" TEXT="A look inside at Google">
+<node CREATED="1156999065375" ID="_" MODIFIED="1156999066203" TEXT="http://video.google.com/videoplay?docid=-8618166999532839788&amp;q=google+recruiting+video&amp;time=30000"/>
+</node>
+</node>
+</node>
+</map>
